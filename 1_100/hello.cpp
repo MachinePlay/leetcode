00adb43e7e0886298bca3c27b3bf552c2ac4c96b
yepmbp <mattbaisteins@gmail.com>
2019-12-02 22:44:12 +0800

    socket and 8 chapter

--- 1_100/hello.cpp
+++ 1_100/hello.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int  main() {
 	cout << "hello world" <<  endl;
 	cout << "hello world" << endl ;
-	return 0;
+	cout << "hello world" << endl;
+    cout << "socket and 8 chapter" << endl;
+    return 0;
 	}
